dd2e7fcfb19ada63e3f141bea5db52038f32c7c3
RossShelashskyi <ross.shelashskyi@tcu.edu>
2023-05-20 10:39:58 -0500

    first solution

--- C++/LongestSubstring/LongestSubstring.cpp
+++ C++/LongestSubstring/LongestSubstring.cpp
@@ -1,10 +1,25 @@
 #include <iostream>
 #include <string.h>
+#include <vector>
+#include <map>
+#include <algorithm>
 using namespace std;
 
 //return the length of longest substring without repeating characters
 int lengthOfLongestSubstring(string s){
-
+    vector<char, char> sublen;
+    for(int i = 0; i < s.length(); i++){
+        map<char, char> mp;
+        for(int j = 0; j < s.length(); j++){
+            if(mp.count(s.at(j))){
+                sublen.push_back(j);
+                break;
+            }
+            mp.insert(pair<char, char>(s.at(j), s.at(j)));
+        }
+    }
+    sort(sublen.begin(), sublen.end());
+    return sublen.back();
 }
 
 int main(){
