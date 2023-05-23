ad9aae2b30745e7fe97d98b390dc9a9f4801d34a
RossShelashskyi <ross.shelashskyi@tcu.edu>
2023-05-22 20:00:21 -0500

    back on track, how am i this stupid

---
+++ C++/LongestSubstring/LongestSubstring.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string.h>
+#include <vector>
+#include <map>
+#include <algorithm>
+using namespace std;
+
+//return the length of longest substring without repeating characters
+int lengthOfLongestSubstring(string s){
+    if(s.length() == 0) return 0;
+    if(s.length() == 1) return 1;
+    vector<int> sublen;
+    map<char, char> mp;
+    for(int i = 0; i < s.length(); i++){
+        mp.clear();
+        for(int j = i; j < s.length(); j++){
+            if(mp.count(s.at(j))){
+                sublen.push_back(j - i);
+                break;
+            }
+            mp.insert(pair<char, char>(s.at(j), s.at(j)));
+        }
+    }
+    sort(sublen.begin(), sublen.end());
+    return sublen.back();
+}
+
+int main(){
+    int a = lengthOfLongestSubstring("abcabcbb");
+    cout << a << endl;
+    return 0;
+}
