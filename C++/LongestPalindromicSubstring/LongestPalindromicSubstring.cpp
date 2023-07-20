e303a7edc0009b052e76512de0642fcc2626af3c
Ros Shelashskyi <ross.shelashskyi@tcu.edu>
2023-07-20 14:00:41 -0500

    first solution (incomplete)

--- C++/LongestPalindromicSubstring/LongestPalindromicSubstring.cpp
+++ C++/LongestPalindromicSubstring/LongestPalindromicSubstring.cpp
@@ -4,32 +4,40 @@
 using namespace std;
 
 int main(){
-
+    string s = "tacocat";
+    cout << FindLongestPalindrome(s, 0) << endl;
 }
 
-string longestPalindrome(string s){
-    vector<string> palindromes;
+string FindLongestPalindrome(string s, int index){
+    string longest = "";
 
-    if(s.length() % 2 == 0){
-        palindromes.push_back(evenPalindrome(s));
+    if(index == 0) return s.substr(0, 1);
+    if(index == s.length() - 1) return s.substr(s.length() - 1, 1);
+
+    for(int i = 1; index - i > 0 && index + i <= s.length(); i++){
+        if(s.at(index - i) != s.at(index + 1)) break;
+        longest = s.substr(index - i + 1, i * 2 + 1);
+    }
+
+    if(longest.length() > FindLongestPalindrome(s, index++).length()){
+        return longest;
     }else{
-        palindromes.push_back(oddPalindrome(s));
+        FindLongestPalindrome(s, index++);
     }
-
 }
 
-string evenPalindrome(string s){
+// string evenPalindrome(string s){
 
-}
+// }
 
-string oddPalindrome(string s){
-    string result = "";
-    int middle = s.length() / 2 + 1;
-    for(int i = 1; i < s.length() - middle; i++){
-        if(s.at(middle - i) != s.at(middle + i)){
-            return result;
-        }
-        result = s.substr(middle - i, 2 * i + 1);
-    }
-    return result;
-}
+// string oddPalindrome(string s){
+//     string result = "";
+//     int middle = s.length() / 2 + 1;
+//     for(int i = 1; i < s.length() - middle; i++){
+//         if(s.at(middle - i) != s.at(middle + i)){
+//             return result;
+//         }
+//         result = s.substr(middle - i, 2 * i + 1);
+//     }
+//     return result;
+// }
