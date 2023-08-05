31f0097f5f75aff7192e94f8c51f432594234225
Rostyslav Shelashskyi <ross.shelashskyi@tcu.edu>
2023-08-05 17:56:43 -0400

    base cases passed + 1 char fix

--- C++/LongestPalindromicSubstring/LongestPalindrome.cpp
+++ C++/LongestPalindromicSubstring/LongestPalindrome.cpp
@@ -4,30 +4,17 @@
 #include <string>
 using namespace std;
 
+string longestPalindrome(string s);
+
 int main(){
-
-}
-
-string LongestPalindrome(string s){
-    string longest;
-
-    for(int i = 1; i < s.length(); i++){
-        if(s.at(i) == s.at(i + 1)){
-            string temp = GetEvenPalindrome(s, i);
-            if(temp.length() > longest.length()) longest = temp;
-        }
-        if(s.at(i) == s.at(i + 2)){
-            string temp = GetOddPalindrome(s, i);
-            if(temp.length() > longest.length()) longest = temp;
-        }
-    }
-
-    return longest;
+    string input = "babad";
+    cout << longestPalindrome(input) << endl;
+    return 0;
 }
 
 string GetEvenPalindrome(string s, int index){
     string palindrome = s.substr(index, 2);
-    for(int i = 1; i < s.length() && index - i >= 0; i++){
+    for(int i = 1; i * 2 < s.length() && index - i >= 0; i++){
         if(s.at(index - i) == s.at(index + 1 + i)){
             palindrome = s.substr(index - i, i * 2);
         }else{
@@ -40,9 +27,11 @@ string GetEvenPalindrome(string s, int index){
 
 string GetOddPalindrome(string s, int index){
     string palindrome = s.substr(index, 3);
-    for(int i = 1; i < s.length() && index - 1 >= 0; i++){
+    for(int i = 1; i * 2< s.length() && index - i >= 0; i++){
+        cout << "i:" << i << endl;
         if(s.at(index - i) == s.at(index + 2 + i)){
-            palindrome = s.substr(index - i, i * 2 + 1);
+            cout << "yeye" << endl;
+            palindrome = s.substr(index - i, (i + 1)* 2 + 1);
         }else{
             return palindrome;
         }
@@ -50,3 +39,24 @@ string GetOddPalindrome(string s, int index){
 
     return palindrome;
 }
+
+string longestPalindrome(string s){
+    string longest;
+
+    if(s.length() == 1) return s;
+
+    for(int i = 0; i + 1 < s.length(); i++){
+        cout << "i: " << i << endl;
+        if(s.at(i) == s.at(i + 1)){
+            string temp = GetEvenPalindrome(s, i);
+            if(temp.length() > longest.length()) longest = temp;
+        }
+        if(i + 2 < s.length() && s.at(i) == s.at(i + 2)){
+            string temp = GetOddPalindrome(s, i);
+            if(temp.length() > longest.length()) longest = temp;
+        }
+    }
+
+    return longest;
+}
+
