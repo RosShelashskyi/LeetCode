73be33a46986070bd723dab4c6016a57d2deba9e
Ros <ross.shelashskyi@tcu.edu>
2023-09-29 12:31:34 -0500

    Initial solution (not tested)

---
+++ Easy/ValidParentheses/ValidParenthesis.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+bool isValid(string s){
+    vector<char> stack;                             //using a vector to push and pop open parentheses to/from the stack
+    int paren = 0;                                  //variables to keep count of parentheses, curved brackets, and square brackets
+    int curved = 0;
+    int square = 0;
+    for(int i = 0; i < s.length(); i++){
+        switch(s.at(i)){
+            case '(':
+                stack.push_back('(');
+                paren++;
+                break;
+            case '{':
+                stack.push_back('{');
+                curved++;
+                break;
+            case '[':
+                stack.push_back('[');
+                square++;
+                break;
+            case ')':
+                if(stack.back() != '(') return false;
+                paren--;
+                stack.pop_back();
+                break;
+            case '}':
+                if(stack.back() != '{') return false;
+                curved--;
+                stack.pop_back();
+                break;
+            case ']':
+                if(stack.back() != '[') return false;
+                square--;
+                stack.pop_back();
+                break;
+        }
+    }
+    return (paren == 0 && curved == 0 && square == 0);
+}
